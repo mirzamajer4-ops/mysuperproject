2097c1f04d2c632b81327dee1de65fb8eb6c675f
Mirza <majer@example.com>
2026-03-31 00:28:03 -0700

    Доделать

--- database/migrations/2026_03_31_070314_create_users_table.php
+++ database/migrations/2026_03_31_070314_create_users_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->Firstname();
+            
             $table->timestamps();
         });
     }
